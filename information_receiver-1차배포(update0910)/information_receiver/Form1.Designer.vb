5ef522fde5f878b2448d29d6a05996a1509a9717
Yeran25 <41954236+Yeran25@users.noreply.github.com>
2018-09-10 22:42:25 +0900

    Add files via upload

---
+++ information_receiver-1차배포(update0910)/information_receiver/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(364, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(69, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "게시판 선택"
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.LabelWrap = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 53)
+        Me.ListView1.MultiSelect = False
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(594, 228)
+        Me.ListView1.TabIndex = 4
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "No."
+        Me.ColumnHeader1.Width = 33
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "제목"
+        Me.ColumnHeader2.Width = 369
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "등록일자"
+        Me.ColumnHeader3.Width = 72
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "등록자명"
+        Me.ColumnHeader4.Width = 115
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ComboBox1.DisplayMember = "1"
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"대학공지사항", "학사공지", "장학공지", "-------공과대학---------", "기계시스템디자인공학과", "기계·자동차공학과", "안전공학과", "신소재공학과", "건축산업학과", "기계설비공학과", "시설물유지관리공학과", "건설시스템공학과", "건축환경설비공학과", "건축공학전공", "건축학전공", "-----정보통신대학-------", "전기정보공학과", "전자IT미디어공학과", "컴퓨터공학과", "----에너지바이오대학----", "화공생명공학과", "환경공학과", "식품공학과", "정밀화학과", "스포츠과학과", "안경광학과", "--------조형대학--------", "디자인학과", "도예학과", "금속공예디자인학과", "조형예술학과", "-----인문사회대학------", "영어영문학과", "행정학과", "문예창작학과", "기초교육학부", "----기술경영융합대학----", "산업정보시스템전공", "MSDE전공", "ITM전공", "경영학전공", "글로벌테크노경영전공", "--------부속시설--------", "미래융합대학", "메이커스 칼리지 ", "창업지원단", "공학교육혁신센터", "국제교육본부"})
+        Me.ComboBox1.Location = New System.Drawing.Point(439, 27)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(167, 20)
+        Me.ComboBox1.TabIndex = 6
+        Me.ComboBox1.Tag = ""
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.Label2.Location = New System.Drawing.Point(360, 284)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(246, 12)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Copyright ©  2018. ITS, All rights reserved."
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.information_receiver.My.Resources.Resources.STECH1
+        Me.PictureBox2.Location = New System.Drawing.Point(60, 10)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(175, 34)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 0
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.information_receiver.My.Resources.Resources.its1
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(46, 44)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(618, 302)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("굴림", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "SeoulTech Notice receiver"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader3 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader4 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ information_receiver-1차배포(update0910)/information_receiver/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+    Dim Winhttp As New WinHttp.WinHttpRequest
+    Dim Url As String = "http://www.seoultech.ac.kr/service/info/notice/"
+
+    '개발자 정보
+    '서울과학기술대학교 컴퓨터공학과 ITS
+    'Blog : http://its319.tistory.com/
+    'E-mail : ehn1225@seoultech.ac.kr
+    '제작년일 : 2018-09-05
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        Select Case ComboBox1.SelectedIndex
+            Case 0
+                Url = "http://www.seoultech.ac.kr/service/info/notice/"
+            Case 1
+                Url = "http://www.seoultech.ac.kr/service/info/matters/"
+            Case 2
+                Url = "http://www.seoultech.ac.kr/service/info/janghak/"
+            Case 3  '공과대학
+                Url = "http://engineer.seoultech.ac.kr/community/notice/"
+            Case 4
+                Url = "http://msd.seoultech.ac.kr/information/bulletin/"
+            Case 5
+                Url = "http://mae.seoultech.ac.kr/bachelor/bulletin/"
+            Case 6
+                Url = "http://safety.seoultech.ac.kr/b_information/notic/"
+            Case 7
+                Url = "http://mse.seoultech.ac.kr/b_intro/notice/"
+            Case 8
+                Url = "http://inarc.seoultech.ac.kr/ba/news/"
+            Case 9
+                Url = "http://mee.seoultech.ac.kr/info/news/"
+            Case 10
+                Url = "http://fm.seoultech.ac.kr/info/news/"
+            Case 11
+                Url = "http://civil.seoultech.ac.kr/haksainfor/bulletin/"
+            Case 12
+                Url = "http://ebse.seoultech.ac.kr/info/notice/"
+            Case 13
+                Url = "http://architecture.seoultech.ac.kr/bachelor_of_information/notice/"
+            Case 14
+                Url = "http://archidesign.seoultech.ac.kr/bachelor_information/notice/"
+            Case 15  '정통대
+                Url = "http://ice.seoultech.ac.kr/community/notice/bulletin/"
+            Case 16
+                Url = "http://eie.seoultech.ac.kr/majornotice/notice/"
+            Case 17
+                Url = "http://eeme.seoultech.ac.kr/bachelor_info/notice/"
+            Case 18  '컴퓨터공학과
+                Url = "http://computer.seoultech.ac.kr/info_plaza/notic/"
+            Case 19  '에바대
+                Url = "http://nature.seoultech.ac.kr/community/notic/"
+            Case 20
+                Url = "http://che.seoultech.ac.kr/engineering_certification/board/"
+            Case 21
+                Url = "http://enviro.seoultech.ac.kr/information/notice/"
+            Case 22
+                Url = "http://food.seoultech.ac.kr/bachelor/notice/"
+            Case 23
+                Url = "http://fchem.seoultech.ac.kr/bachelor_of_information_/notice_/"
+            Case 24
+                Url = "http://sports.seoultech.ac.kr/b_information/notic/"
+            Case 25
+                Url = "http://optometry.seoultech.ac.kr/bachelor_of_information/notice/"
+            Case 26  '조형대학
+                Url = "http://and.seoultech.ac.kr/advertisements/notice/"
+            Case 27
+                Url = "http://design.seoultech.ac.kr/square/bulletin/"
+            Case 28
+                Url = ""
+                Process.Start("https://ceramic.seoultech.ac.kr/html/board/list.php?board=11")
+            Case 29
+                Url = "http://metalartdesign.seoultech.ac.kr/b_information/notic/"
+            Case 30
+                Url = "http://fineart.seoultech.ac.kr/community/notice/"
+            Case 31  '인사대
+                Url = "http://human.seoultech.ac.kr/community/notice/"
+            Case 32
+                Url = "http://eng.seoultech.ac.kr/information_plaza/notice/"
+            Case 33
+                Url = "http://pa.seoultech.ac.kr/admini/notice/"
+            Case 34
+                Url = "http://writing-creative.seoultech.ac.kr/bachelor_of_information_/notice_/"
+            Case 35
+                Url = "http://liberal.seoultech.ac.kr/community/notic/"
+            Case 36  '기술경영융합
+                Url = "http://sgc.seoultech.ac.kr/open/notice/"
+            Case 37
+                Url = "http://iise.seoultech.ac.kr/notice/faculty_announcements_/"
+            Case 38
+                Url = "http://msde.seoultech.ac.kr/academics_/qna/"
+            Case 39
+                Url = "http://itm.seoultech.ac.kr/bachelor_of_information/notice/"
+            Case 40
+                Url = "http://biz.seoultech.ac.kr/info_plaza/notic/"
+            Case 41
+                Url = "http://gtm.seoultech.ac.kr/info_plaza/gtm_info/"
+            Case 42  '부속시설
+                Url = ""
+                Process.Start("http://www.seoultech.ac.kr/life/attatched/press/")
+            Case 43
+                Url = "http://m-disciplinary.seoultech.ac.kr/community/notice/"
+            Case 44
+                Url = "http://mc.seoultech.ac.kr/community/notice/"
+            Case 45
+                Url = "http://sssf.seoultech.ac.kr/community/notice/"
+            Case 46
+                Url = "http://icee.seoultech.ac.kr/bbs/notice/"
+            Case 47
+                Url = ""
+                Process.Start("http://global.seoultech.ac.kr/")
+        End Select
+
+        If Not Url = "" Then
+            Roadpage(ComboBox1.SelectedIndex)
+        End If
+
+    End Sub
+
+    Private Sub Roadpage(SelectedIndex As Integer)
+        ListView1.Items.Clear()
+
+        Dim temp As String
+        Dim countnotice As Integer
+
+        Winhttp.Open("GET", Url)
+        Winhttp.Send()
+        countnotice = UBound(Split(Winhttp.ResponseText, "<tr class=""body_tr"">")) '공지사항의 개수를 받아옴.]
+        If SelectedIndex < 3 And Not countnotice = 0 Then
+            For i = 1 To countnotice
+                temp = Split(Split(Winhttp.ResponseText, "<tr class=""body_tr"">")(i), "</tr>")(0) 'source에 공지사항을 받아옴.
+                ListView1.Items.Add(i)
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, ">")(4 + UBound(Split(temp, "ico_notice_ko.gif"))), "<")(0))
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<td style=""text-align: center;"">")(3), "</td>")(0)) '게시일자
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<td style=""text-align: center;"">")(4), "</td>")(0)) '게시자
+                ListView1.Items(i - 1).SubItems.Add(Url)
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<a href='")(1), "'>")(0))
+            Next
+        ElseIf SelectedIndex >= 3 And Not countnotice = 0 Then
+            For i = 1 To countnotice
+                temp = Split(Split(Winhttp.ResponseText, "<tr class=""body_tr"">")(i), "</tr>")(0) 'temp에 공지사항을 받아옴.
+                ListView1.Items.Add(i)
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, ">")(5 + UBound(Split(temp, "ico_notice_ko.gif"))), "<")(0))
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<td class=""body_col_regdate"" align=""center"">")(1), "</td>")(0))
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<td class=""body_col_user"" align=""center"">")(2), "</td>")(0))
+                ListView1.Items(i - 1).SubItems.Add(Url)
+                ListView1.Items(i - 1).SubItems.Add(Split(Split(temp, "<a href='")(1), "'>")(0))
+            Next
+        Else
+            MsgBox("공지사항이 없습니다.")
+        End If
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        Process.Start("http://www.seoultech.ac.kr/index.jsp")
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Process.Start("http://its319.tistory.com/")
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        MessageBox.Show("개발 : ITS" & Chr(13) & "E-Mail : ehn1225@seoultech.ac.kr" & Chr(13) & "개발 및 배포일자 : 2018-09-05", "개발자정보", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ListView1_DoubleClick(sender As Object, e As EventArgs) Handles ListView1.DoubleClick
+        Process.Start(ListView1.Items(ListView1.FocusedItem.Index).SubItems(4).Text & ListView1.Items(ListView1.FocusedItem.Index).SubItems(5).Text)
+
+    End Sub
+End Class
